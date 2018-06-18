3e264d5f6d4f4784c028f6ce837467fbb3c4b383
steve <steve@fncserver3>
2018-06-18 19:30:22 -0400

    1

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -46,7 +46,7 @@
                     echo "STP Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
                     sh "echo 'STP building ${config.projectName} ...'"
 					
-					withMaven()
+					withMaven(maven: 'Maven3.5')
 					   { sh "mvn -f Maven_Template_Global/ -B install" }
 					
 					
